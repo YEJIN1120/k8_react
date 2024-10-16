d41cc72206f0f3713f8d2d627e5f49b419d72064
YEJIN1120 <kyj21120@naver.com>
2024-10-16 09:31:48 +0900

    일기예보

---
+++ src/14/Fcst.js
@@ -0,0 +1,70 @@
+import TailButton from "../UI/TailButton" ;
+import getxy from './getxy.json';
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+
+export default function Fcst() {
+  const sido = getxy.map(item => item["1단계"])
+                    .map(item => <option key={item} value={item}>
+                                 {item}
+                                 </option>);
+
+  const navigate = useNavigate();
+
+  //날짜
+  const txtDt = useRef();
+  //지역
+  const txtArea = useRef();
+
+  console.log(sido)
+
+  //버튼이 눌러졌을 때
+  const handleOk = (gubun) => {
+    if(txtDt.current.value === '') {
+      alert('날짜를 선택하세요.')
+      txtDt.current.focus();
+      return;
+    }
+    if(txtArea.current.value === '') {
+      alert('지역을 선택하세요.')
+      txtArea.current.focus();
+      return;
+    }
+    console.log(gubun)
+    const dt = txtDt.current.value.replaceAll('-','');
+    const loc = getxy.filter(item => item["1단계"] === txtArea.current.value)[0];
+    const x = loc["격자 X"];
+    const y = loc["격자 Y"];
+
+
+    navigate(`/fcstlist?gubun=${gubun}&dt=${dt}&x=${x}&y=${y}&area=${txtArea.current.value}`);
+  }
+
+  return (
+    <div className = "w-full flex flex-col justify-center items-center ">
+      <h1 className = "w-full text-center items-center text-3xl my-10 font-bold">
+      일기예보 선택
+      </h1>
+      <div className = "w-10/12 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <input type = "date"
+               ref = {txtDt}
+               className = "form-input"
+               id = 'txt1'/>
+        <select className = "form-select"
+                ref = {txtArea}>
+          <option value = '' >-- 지역을 선택하세요.</option>
+          {sido}
+        </select>
+        <TailButton caption = '초단기예보'
+                    color = 'blue'
+                    handleClick = {() => handleOk('초단기예보')}
+                    size = 'w-1/2'/>
+        <TailButton caption = '단기예보'
+                    color = 'blue'
+                    handleClick = {() => handleOk('단기예보')}
+                    size = 'w-1/2'/>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/14/FcstList.js
@@ -0,0 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
+export default function FcstList() {
+  const [sParams] = useSearchParams();
+  const gubun = sParams.get('gubun');
+  const dt = sParams.get('dt');
+  const x = sParams.get('x');
+  const y = sParams.get('y');
+  const area = sParams.get('area');
+  console.log(gubun, dt, x, y, area);
+
+  return (
+    <div>
+      <div className = "flex ">
+
+      </div>
+      <select>
+        
+      </select>
+    </div>
+  ) 
+}
